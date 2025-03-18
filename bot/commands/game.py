d58a0a742c24028102356cef3e197e8dd3cb0d14
izana <izana.khabouri@univ-rouen.fr>
2025-03-18 18:01:36 +0100

    feat: manage guilds by members

--- bot/commands/game.py
+++ bot/commands/game.py
@@ -9,56 +9,60 @@ class Game(commands.Cog):
 
     @commands.command()
     async def add(self, ctx, *, member: discord.Member = None):
-        member = member or ctx.author
         """Add new members in list of members"""
+        member = member or ctx.author
+        guild = ctx.message.guild.id
         index = self.index_of_member(member)
         if index == -1 and member != None:
             self._members.append(Member(member))
-            await ctx.send(f'{member.name} est ajouté à la liste des participants')
+            self._members[-1].set_is_play(True, guild)
         else:
             m = self._members[index]
-            if not m.get_is_play():
-                m.set_is_play(True)
-                await ctx.send(f'{member.name} est a nouveau dans la liste des participants')
-            else:
-                await ctx.send(f'{member.name} fait déjà partie des participants')
+            m.set_is_play(True, guild)
+        await ctx.send(f'{member.name} est maintenant prêt à jouer dans {ctx.message.guild.name}')
 
     @commands.command()
     async def rem(self, ctx, *, member: discord.Member = None):
-        member = member or ctx.author
         """Remove members of the game"""
+        member = member or ctx.author
+        guild = ctx.message.guild.id
         index = self.index_of_member(member)
         if index != -1 and member != None:
             m = self._members[index]
-            if m.get_is_play():
-                m.set_is_play(False)
-                await ctx.send(f'{member.name} est retiré de la liste des participants')
+            if m.contain_guild(guild):
+                if m.get_is_play(guild):
+                    m.set_is_play(False, guild)
+                    await ctx.send(f'{member.name} se retire de la compétition dans {ctx.message.guild.name}')
             else:
-                await ctx.send(f'{member.name} s\' déjà retiré des participants')
-        else:
-            await ctx.send(f'{member.name} n\'a jamais participé')
+                await ctx.send(f'{member.name} n\'a jamais joué dans {ctx.message.guild.name}')
 
     @commands.command()
     async def delete(self, ctx, *, member: discord.Member = None):
+        """Remove members of the game"""
         member = member or ctx.author
-        """Remove definitely members in list of members"""
+        guild = ctx.message.guild.id
         index = self.index_of_member(member)
         if index != -1 and member != None:
-            self._members.pop(index)
-            await ctx.send(f'{member.name} est retiré de la liste des membres')
-        else:
-            await ctx.send(f'{member.name} n\'a jamais participé')
+            m = self._members[index]
+            if m.contain_guild(guild):
+                if m.get_is_play(guild):
+                    self._members.pop(index)
+                    await ctx.send(f'{member.name} ne fait maintenant plus parti des joueurs dans {ctx.message.guild.name}')
+            else:
+                await ctx.send(f'{member.name} n\'a jamais joué dans {ctx.message.guild.name}')
 
     @commands.command()
     async def rank(self, ctx):
         """Display rank"""
-        if not self._members:
-            await ctx.send("Aucun membre dans le classement.")
+        guild = ctx.message.guild.id
+        members = self.members_by_guild(guild)
+        if len(members) == 0:
+            await ctx.send(f"Aucun membre dans le classement dans {ctx.message.guild.name}")
             return
-        members = sorted(self._members, key=lambda m: m.get_points(), reverse=True)
+        members = sorted(members, key=lambda m: m.get_points(), reverse=True)
         text = ""
         for m in members:
-            playing = "Participe" if m.get_is_play() else "Ne participe plus"
+            playing = "Participe" if m.get_is_play(guild) else "Ne participe plus"
             text += f"{m.get_member().name} - {m.get_points()} - {playing} \n"
         await ctx.send(text)
 
@@ -67,3 +71,10 @@ class Game(commands.Cog):
             if member == m.get_member():
                 return self._members.index(m)
         return -1
+
+    def members_by_guild(self, guild):
+        members = []
+        for m in self._members:
+            if m.contain_guild(guild):
+                members.append(m)
+        return members
--- bot/entities/member.py
+++ bot/entities/member.py
@@ -4,16 +4,22 @@ class Member():
     def __init__(self, member: discord.Member):
         self._member = member
         self._points = 0
-        self._play = True
+        self._guilds_play = {}
 
-    def get_is_play(self):
-        return self._play
+    def contain_guild(self, guild):
+        return guild in self._guilds_play
 
-    def set_is_play(self, playing):
-        self._play = playing
+    def get_is_play(self, guild):
+        return True if self._guilds_play[guild] else False
+
+    def set_is_play(self, playing, guild):
+        self._guilds_play[guild] = playing
 
     def get_member(self):
         return self._member
 
     def get_points(self):
         return self._points
+
+    def add_points(self, points : int):
+        self._points += 10
