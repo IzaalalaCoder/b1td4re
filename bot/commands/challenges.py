f03ebbcba68e9b416380c843089d557bdf433045
izana <izana.khabouri@univ-rouen.fr>
2025-03-21 21:52:40 +0100

    feat: add command to read daily challenges

--- bot/commands/challenges.py
+++ bot/commands/challenges.py
@@ -1,45 +1,17 @@
 import discord
 from discord.ext import commands
 from commands.game import Game
-
-import xml.etree.ElementTree as ET
-
-# Chargement du fichier XML
+from entities.challenge import Challenge
 
 class Challenges(commands.Cog):
     def __init__(self, bot, game : Game):
         self.bot = bot
         self._game = game
-
-    # Récupération des informations spécifiques
-    def get_challenge_details(self):
-        tree = ET.parse('bot/assets/april/2025.xml')  # Remplacer par le chemin de votre fichier XML
-        root = tree.getroot()
-        # Trouver le challenge du jour 1 (ou autre jour selon votre besoin)
-        challenge = root.find(".//challenge[@jour='1']")
-
-        if challenge is not None:
-            # Récupérer les informations spécifiques du challenge
-            titre = challenge.find("titre").text
-            explication = challenge.find("explication").text
-            description = challenge.find("description").text
-
-            # Récupérer les critères de succès
-            criteres_succes = []
-            for critere in challenge.findall(".//criteres_succes/critere"):
-                criteres_succes.append(critere.text)
-
-            # Affichage des résultats
-            text = f"Titre: {titre}\nExplication: {explication}\nDescription: {description} \nCritères de succès:"
-            for critere in criteres_succes:
-                text += f"\n- {critere}"
-            return text
-        else:
-            return "Le challenge pour le jour spécifié n'a pas été trouvé."
+        self._challenge = Challenge()
 
     @commands.command()
     async def today(self, ctx, *, member: discord.Member = None):
         """Add new members in list of members"""
         guild = ctx.message.guild.id
         if self._game.get_all_members()[self._game.index_of_member(member)].get_is_play(guild):
-            await ctx.send(f'{self.get_challenge_details()}')
+            await ctx.send(f'{self._challenge.get_all_challenges_on_day()}')
--- bot/entities/challenge.py
+++ bot/entities/challenge.py
@@ -1,12 +1,27 @@
+import datetime as d
+import xml.etree.ElementTree as ET
+
 class Challenge():
-    def __init__(self, ):
-        self._member = member
-        self._points = 0
-        self._guilds_play = {}
+    def _get_correct_file_on_today(self):
+        date = d.date.today()
+        month = date.strftime("%B").lower()
+        year = date.year
+        return ET.parse(f'bot/assets/{month}/{year}.xml')
 
+    def get_all_challenges_on_day(self):
+        root = self._get_correct_file_on_today().getroot()
+        day = d.date.today().day
+        challenge = root.find(f".//challenge[@jour='{day}']")
+        if challenge is not None:
+            titre = challenge.find("titre").text
+            explication = challenge.find("explication").text
+            description = challenge.find("description").text
+            text = f"Titre: {titre}\nExplication: {explication}\nDescription: {description} \nCritères de succès:"
+            for critere in challenge.findall(".//criteres_succes/critere"):
+                text += f"\n- {critere.text}"
+            return text
+        else:
+            return "Le challenge pour le jour spécifié n'a pas été trouvé."
 
-    def get_challenges_on_day(self):
-        pass
-
-    def get_challenge_on_day(self, index : int):
+    def get_one_challenge_on_day(self, index : int):
         pass
