2aa55264564895167b48b8e20b5d353af5534334
izana <izana.khabouri@univ-rouen.fr>
2025-03-22 13:49:09 +0100

    feat: add command to read specific challenge

--- bot/commands/challenges.py
+++ bot/commands/challenges.py
@@ -9,9 +9,26 @@ class Challenges(commands.Cog):
         self._game = game
         self._challenge = Challenge()
 
+    def is_numeric(self, index):
+        is_numeric = True
+        for c in index:
+            if not 47 < ord(c) < 58:
+                is_numeric = False
+                break
+        return is_numeric
+
     @commands.command()
     async def today(self, ctx, *, member: discord.Member = None):
         """Add new members in list of members"""
         guild = ctx.message.guild.id
         if self._game.get_all_members()[self._game.index_of_member(member)].get_is_play(guild):
             await ctx.send(f'{self._challenge.get_all_challenges_on_day()}')
+
+    @commands.command()
+    async def challenge(self, ctx, *index, member: discord.Member = None):
+        if len(index) != 1 or not self.is_numeric(index[0]):
+            await ctx.send("Aucun challenge n'a été spécifié")
+            return
+        guild = ctx.message.guild.id
+        if self._game.get_all_members()[self._game.index_of_member(member)].get_is_play(guild):
+            await ctx.send(f'{self._challenge.get_one_challenge_on_day(int(index[0]))}')
--- bot/entities/challenge.py
+++ bot/entities/challenge.py
@@ -24,4 +24,16 @@ class Challenge():
             return "Le challenge pour le jour spécifié n'a pas été trouvé."
 
     def get_one_challenge_on_day(self, index : int):
-        pass
+        root = self._get_correct_file_on_today().getroot()
+        day = d.date.today().day
+        challenge = root.find(f".//challenge[@jour='{day}'][@index='{index}']")
+        if challenge is not None:
+            titre = challenge.find("titre").text
+            explication = challenge.find("explication").text
+            description = challenge.find("description").text
+            text = f"Titre: {titre}\nExplication: {explication}\nDescription: {description} \nCritères de succès:"
+            for critere in challenge.findall(".//criteres_succes/critere"):
+                text += f"\n- {critere.text}"
+            return text
+        else:
+            return f"Le challenge pour le jour numéro {index} spécifié n'a pas été trouvé."
