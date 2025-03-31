88c8976c9e99576b20665c81fceda9087fb8c103
izana <iza.khabouri@gmail.com>
2025-04-01 00:08:11 +0200

    feature: add method to return calendar

--- bot/entities/member.py
+++ bot/entities/member.py
@@ -1,5 +1,6 @@
 import discord
-import datetime as d
+import datetime
+import calendar as cal
 
 class Member:
     def __init__(self, member: discord.Member):
@@ -17,6 +18,17 @@ class Member:
     def set_is_play(self, playing, guild):
         self._guilds_play[guild] = playing
 
+    def get_calendar_playing_by_guild(self, guild):
+        date = datetime.date.today()
+        month = date.month
+        year = date.year
+
+        playings = []
+        for d in range(1, cal.monthrange(year, month)[1] + 1):
+            key = f"{d}/{month}/{year}--{guild}"
+            playings.append(key in self._calendar_playing)
+        return playings, cal.monthrange(year, month)[0]
+
     def get_member(self):
         return self._member
 
@@ -27,7 +39,7 @@ class Member:
         self._points += points
 
     def add_participate(self, id : int, guild):
-        date = d.date.today()
+        date = datetime.date.today()
         month = date.month
         year = date.year
         day = date.day
@@ -39,7 +51,7 @@ class Member:
         self._calendar_playing[key].append(id)
 
     def has_realized(self, id : int, guild):
-        date = d.date.today()
+        date = datetime.date.today()
         month = date.month
         year = date.year
         day = date.day
